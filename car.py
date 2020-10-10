c4ed97482b0138eac9caebbe5344f982878965a7
shift-right <b34064061@gs.ncku.edu.tw>
2020-10-10 19:09:40 +0800

    inital commit

---
+++ car.py
@@ -0,0 +1,150 @@
+#import time
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import core.utils as utils
+from core.yolov4 import filter_boxes
+from tensorflow.python.saved_model import tag_constants
+from PIL import Image
+import cv2
+import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+
+import socket
+import sys
+import cv2
+import pickle
+import struct
+
+HOST = '192.168.137.1'
+PORT = 8000
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+s.bind((HOST, PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+conn, addr = s.accept()
+
+
+
+flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
+flags.DEFINE_string('weights', './checkpoints/yolov4-416',
+                    'path to weights file')
+flags.DEFINE_integer('size', 416, 'resize images to')
+flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+flags.DEFINE_string('video', './data/video/video.mp4', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('output', None, 'path to output video')
+flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_float('iou', 0.45, 'iou threshold')
+flags.DEFINE_float('score', 0.25, 'score threshold')
+flags.DEFINE_boolean('dont_show', False, 'dont show video output')
+
+def main(_argv):
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = FLAGS.size
+    video_path = FLAGS.video
+
+    
+
+    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
+
+
+
+    while True:
+
+        data = b''
+        payload_size = struct.calcsize("=L")
+
+
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+        packed_msg_size = data[:payload_size]
+
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        frame_read=pickle.loads(frame_data)
+       
+        frame = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(frame)
+    
+        frame_size = frame.shape[:2]
+        image_data = cv2.resize(frame, (input_size, input_size))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+        #start_time = time.time()
+        
+        batch_data = tf.constant(image_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=FLAGS.iou,
+            score_threshold=FLAGS.score
+        )
+        pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+        image,x_mid,y_mid,class_name = utils.draw_bbox(frame, pred_bbox)
+        
+        print("x_mid:",x_mid)
+        print("y_mid:",y_mid)
+        print("class_name:",class_name)
+
+        for i in range(5):
+            if class_name[i]==0:
+                if (x_mid[i]-0.5)>0.2:
+                    serverMessage = 'right'
+                    conn.send(serverMessage.encode(encoding = "gb2312"))
+                    print("shift right")
+                    break
+                if (x_mid[i]-0.5)<(-0.2):
+                    serverMessage = 'left'
+                    conn.send(serverMessage.encode(encoding = "gb2312"))
+                    print("shift left")
+                    break
+        
+        #fps = 1.0 / (time.time() - start_time)
+        #print("FPS: %.2f" % fps)
+        result = np.asarray(image)
+        cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
+        result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        cv2.imshow("result", result)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+
+
+
+
+
+        
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
